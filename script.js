e05cd47bdda201afa082a8476a118321162eb662
kckuei <kevinkuei@gmail.com>
2022-09-01 00:03:33 -0700

    Refactor code and minor cosmetic ui changes

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-// document poclearinters
 const grid = document.getElementById('grid');
 const colorPicker = document.getElementById('colorpicker');
-const monochromeToggle = document.getElementById('monochrome-toggle');
-const rainbowToggle = document.getElementById('rainbow-toggle');
-const eraseToggle = document.getElementById('erase-toggle');
-const gridlinesToggle = document.getElementById('gridlines-toggle');
+const monoTogg = document.getElementById('monochrome-toggle');
+const rainbowTogg = document.getElementById('rainbow-toggle');
+const eraseTogg = document.getElementById('erase-toggle');
+const gridlinesTogg = document.getElementById('gridlines-toggle');
 const clear = document.getElementById('clear-button');
 const gridSlider = document.getElementById('grid-slider');
 const gridText = document.getElementById('grid-text');
-// const newGrid = document.getElementById('grid-button');
 
-// drawing state variables
 let gridSize = parseInt(gridSlider.value);
 let style = getComputedStyle(grid);
 let width = parseInt(style.width.replace('px', ''));
 let elementSize = width / gridSize;
 let cells;
 let drawColor = colorPicker.value;
-
-// hot key state variables
+let colorGradIndex = 0;
 let eraseMode = false;
 let gridMode = false;
-let monochromeMode = false;
+let monoMode = false;
 let rainbowMode = false;
 
-// sets event listeners
-clear.addEventListener('click', clearCanvas);
-colorPicker.addEventListener('change', pickColor);
-gridSlider.addEventListener('change', setGridSizeFromSlider);
-monochromeToggle.addEventListener('change', toggleMonochrome);
-rainbowToggle.addEventListener('change', toggleRainbow);
-eraseToggle.addEventListener('change', toggleEraser);
-gridlinesToggle.addEventListener('change', toggleGrid);
-window.addEventListener('keydown', checkHotKeys);
-// newGrid.addEventListener('click', setGridSizeFromPrompt);
+clear.onclick = () => reloadGrid();
+colorPicker.onchange = () => pickColor();
+monoTogg.onchange = () => toggleMono();
+rainbowTogg.onchange = () => toggleRainbow();
+eraseTogg.onchange = () => toggleEraser();
+gridlinesTogg.onchange = () => toggleGrid();
+gridSlider.onmousemove = (e) => changeGridSizeVal(e.target.value);
+gridSlider.onchange = (e) => changeGridSize(e.target.value);
+window.onkeydown = (e) => checkHotKeys(e.key);
 
 
-function initializeCanvas(gridSize = 20) {
+function changeGridSizeVal(size) {
+    gridText.innerText = `Grid Size: ${size} x ${size}`;
+    gridSize = size;
+}
+
+function changeGridSize(size) {
+    changeGridSizeVal(size)
+    reloadGrid();
+}
+
+function clearGrid() {
+    grid.innerHTML = '';
+}
+
+function reloadGrid() {
+    clearGrid();
+    createGrid(gridSize);
+}
+
+function createGrid(gridSize) {
+    width = parseInt(style.width.replace('px', ''));
+    elementSize = width / gridSize;
 
     for (let row = 0; row < gridSize; row++) {
 
@@ -48,12 +64,12 @@ function initializeCanvas(gridSize = 20) {
 
             let cell = document.createElement('div');
             cell.className = 'cell';
-            cell.style.transitionDuration = '0.25s';
+            // cell.style.transitionDuration = '0.25s';
             cell.style.display = 'inline-block';
             cell.style.height = `${elementSize}px`;
             cell.style.width = `${elementSize}px`;
-            cell.addEventListener('mouseover', fillCell)
-            cell.addEventListener('click', fillCell);
+            cell.addEventListener('mouseover', changeColor);
+            cell.addEventListener('mousedown', changeColor);
             if (gridMode) {
                 cell.classList.add('cell-grid');
             }
@@ -63,39 +79,22 @@ function initializeCanvas(gridSize = 20) {
     cells = grid.getElementsByClassName('cell');
 }
 
-function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-}
 
-function clearCanvas() {
-    removeAllChildNodes(grid);
-    initializeCanvas(gridSize);
-}
+function changeColor(e) {
+    let mouseDown = (e.which === 1);
+    if (e.type === 'mouseover' && !mouseDown) return;
 
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-
-function fillCell(e) {
-    let mouseIsDown = (e.which === 1);
-    if (mouseIsDown) {
-        if (eraseMode) {
-            e.target.style.backgroundColor = 'white';
+    if (eraseMode) {
+        e.target.style.backgroundColor = 'white';
+    } else {
+        if (rainbowMode) {
+            rgb = nextColorGradient(.3, .3, .3, 0, 2, 4)
+            e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
         } else {
-            if (rainbowMode) {
-                rgb = nextColorGradient(.3, .3, .3, 0, 2, 4)
-                e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-            } else {
-                e.target.style.backgroundColor = drawColor;
-            }
+            e.target.style.backgroundColor = drawColor;
         }
     }
 }
-
-let colorGradIndex = 0;
 
 function nextColorGradient(frequency1, frequency2, frequency3,
     phase1, phase2, phase3,
@@ -120,87 +119,63 @@ function nextColorGradient(frequency1, frequency2, frequency3,
 
 function pickColor() {
     drawColor = colorPicker.value;
-    monochromeMode = true;
-    rainbowMode = false;
-    eraseMode = false;
+    monoMode = false;
+    toggleMono();
 }
 
+// hotkey callbacks
 
-function setGridSizeFromPrompt() {
-    do {
-        response = prompt('Choose a grid-size:', gridSize);
-        response = parseInt(response);
-    } while ((response < 0) || (response > 50))
-    if (!isNaN(response)) {
-        gridText.innerText = `Grid Size: ${gridSize} x ${gridSize}`;
-        gridSize = response;
-        elementSize = width / gridSize;
-        clearCanvas();
-    }
+function checkHotKeys(key) {
+    if (key === 'c') reloadGrid();
+    if (key === 'e') toggleEraser();
+    if (key === 'g') toggleGrid();
+    if (key === 'm') toggleMono();
+    if (key === 'r') toggleRainbow();
 }
 
-function setGridSizeFromSlider() {
-    gridText.innerText = `Grid Size: ${gridSize} x ${gridSize}`;
-    gridSize = parseInt(gridSlider.value);
-    elementSize = width / gridSize;
-    clearCanvas();
-}
-
-
-// hot key callbacks
-
-function checkHotKeys(e) {
-    if (e.key === 'c') clearCanvas();
-    if (e.key === 'e') toggleEraser();
-    if (e.key === 'g') toggleGrid();
-    if (e.key === 'm') toggleMonochrome();
-    if (e.key === 'r') toggleRainbow();
-}
-
-
-function toggleMonochrome() {
+function toggleMono() {
     eraseMode = false
-    eraseToggle.checked = false;
-    if (monochromeMode) {
-        monochromeMode = false
-        eraseToggle.checked = false;
+    eraseTogg.checked = false;
+    if (monoMode) {
+        monoMode = false
+        eraseTogg.checked = false;
     } else {
-        monochromeMode = true;
-        monochromeToggle.checked = true;
+        monoMode = true;
+        monoTogg.checked = true;
         rainbowMode = false;
-        rainbowToggle.checked = false;
+        rainbowTogg.checked = false;
     }
 }
 
 function toggleRainbow() {
     eraseMode = false
-    eraseToggle.checked = false;
+    eraseTogg.checked = false;
     if (rainbowMode) {
         rainbowMode = false
-        rainbowToggle.checked = false;
-        monochromeMode = true;
-        monochromeToggle.checked = true;
+        rainbowTogg.checked = false;
+        monoMode = true;
+        monoTogg.checked = true;
     } else {
         rainbowMode = true;
-        rainbowToggle.checked = true;
-        monochromeMode = false;
-        monochromeToggle.checked = false;
+        rainbowTogg.checked = true;
+        monoMode = false;
+        monoTogg.checked = false;
     }
 }
 
 function toggleEraser() {
     if (eraseMode) {
         eraseMode = false
-        eraseToggle.checked = false;
-        monochromeMode = true;
-        monochromeToggle.checked = true;
+        eraseTogg.checked = false;
+        monoMode = true;
+        monoTogg.checked = true;
     } else {
         eraseMode = true;
-        eraseToggle.checked = true;
+        eraseTogg.checked = true;
         rainbowMode = false
-        rainbowToggle.checked = false;
-        monochromeMode = false;
-        monochromeToggle.checked = false;
+        rainbowTogg.checked = false;
+        monoMode = false;
+        monoTogg.checked = false;
     }
 }
 
@@ -208,14 +183,14 @@ function toggleGrid() {
     if (gridMode) {
         Array.from(cells).forEach(cell => cell.classList.remove('cell-grid'));
         gridMode = false;
-        gridlinesToggle.checked = false;
+        gridlinesTogg.checked = false;
     } else {
         Array.from(cells).forEach(cell => cell.classList.add('cell-grid'));
         gridMode = true;
-        gridlinesToggle.checked = true;
+        gridlinesTogg.checked = true;
     }
 }
 
-
-
-initializeCanvas(gridSize);
+window.onload = () => {
+    createGrid(gridSize);
+}

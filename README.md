d0e100a2fdedfa3076c41e6804e6ce60f26c6a17
kckuei <kevinkuei@gmail.com>
2022-08-31 17:28:33 -0700

    Refactor script.js

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Toy etch-a-sketch project using vanilla `JavaScript`, `HTML`, and `CSS`. Live demo [hero](#). 
 
 ![Sample](./images/sample.png).
+// https://nicoguaro.github.io/posts/cyclic_colormaps/
+// https://krazydad.com/tutorials/makecolors.php
--- script.js
+++ script.js
@@ -1,29 +1,125 @@
-function initializeCanvas(gridSize = 16) {
-    gridSize = parseInt(gridSlider.value);
-    elementSize = width / gridSize;
+// document pointers
+const grid = document.getElementById('grid');
+const colorPicker = document.getElementById('colorpicker');
+const gridSlider = document.getElementById('slider')
+const rainbow = document.getElementById('rainbow');
+const monotone = document.getElementById('monotone')
+const refresh = document.querySelector('#refresh-button');
+const newGrid = document.querySelector('#grid-button');
 
-    for (let i = 0; i < gridSize; i++) {
-        let row = document.createElement("div");
-        row.style.height = `${elementSize}px`;
-        grid.appendChild(row);
-        for (let j = 0; j < gridSize; j++) {
+// drawing state variables
+let gridSize = parseInt(gridSlider.value);
+let style = getComputedStyle(grid);
+let width = parseInt(style.width.replace('px', ''));
+let elementSize = width / gridSize;
+let cells;
+let drawColor = '#ff0000'
+
+// hot key state variables
+let eraseMode = false;
+let gridMode = false;
+let monotoneMode = false;
+let rainbowMode = false;
+
+// sets event listeners
+newGrid.addEventListener('click', setGridSizeFromPrompt);
+refresh.addEventListener('click', refreshCanvas);
+colorPicker.addEventListener('change', pickColor);
+gridSlider.addEventListener('change', setGridSizeFromSlider);
+rainbow.addEventListener('click', toggleRainbow);
+monotone.addEventListener('click', toggleMonotone);
+window.addEventListener('keydown', checkHotKeys);
+
+
+function initializeCanvas(gridSize = 25) {
+
+    for (let row = 0; row < gridSize; row++) {
+
+        let div = document.createElement("div");
+        div.style.height = `${elementSize}px`;
+        grid.appendChild(div);
+
+        for (let col = 0; col < gridSize; col++) {
+
             let cell = document.createElement('div');
-            // cell.innerHTML = `${i},${j}`;
+            cell.className = 'cell';
+            cell.style.transitionDuration = '0.25s';
             cell.style.display = 'inline-block';
             cell.style.height = `${elementSize}px`;
             cell.style.width = `${elementSize}px`;
-            cell.style.transitionDuration = '0.25s';
-            cell.className = 'cell';
             cell.addEventListener('mouseover', fillCell)
             cell.addEventListener('click', fillCell);
             if (gridMode) {
                 cell.classList.add('cell-grid');
             }
-            row.appendChild(cell);
+            div.appendChild(cell);
         }
     }
+    cell.style.transitionDuration = '0.25s';
     cells = grid.getElementsByClassName('cell');
 }
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function refreshCanvas() {
+    removeAllChildNodes(grid);
+    initializeCanvas(gridSize);
+}
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+function fillCell(e) {
+    let mouseIsDown = (e.which === 1);
+    if (mouseIsDown) {
+        if (eraseMode) {
+            e.target.style.backgroundColor = 'rgb(255,255,255)';
+        } else {
+            if (rainbowMode) {
+                rgb = nextColorGradient(.3, .3, .3, 0, 2, 4)
+                e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+            } else {
+                e.target.style.backgroundColor = drawColor;
+            }
+        }
+    }
+}
+
+let colorGradIndex = 0;
+
+function nextColorGradient(frequency1, frequency2, frequency3,
+    phase1, phase2, phase3,
+    center, width, len) {
+
+    if (center == undefined) center = 128;
+    if (width == undefined) width = 127;
+    if (len == undefined) len = 50;
+
+    let red = Math.sin(frequency1 * colorGradIndex + phase1) * width + center;
+    let grn = Math.sin(frequency2 * colorGradIndex + phase2) * width + center;
+    let blu = Math.sin(frequency3 * colorGradIndex + phase3) * width + center;
+
+    if (colorGradIndex > 50) {
+        colorGradIndex = 0;
+    }
+    colorGradIndex++;
+    return [red, grn, blu];
+}
+
+// ui callbacks
+
+function pickColor() {
+    drawColor = colorPicker.value;
+    monotoneMode = true;
+    rainbowMode = false;
+}
+
 
 function setGridSizeFromPrompt() {
     do {
@@ -44,103 +140,10 @@ function setGridSizeFromSlider() {
     refreshCanvas();
 }
 
-function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-        // console.log(parent)
-        parent.removeChild(parent.firstChild);
-    }
-}
 
-function refreshCanvas() {
-    removeAllChildNodes(grid);
-    initializeCanvas(gridSize);
-}
-
-function randomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-
-function fillCell(e) {
-    // console.log(e);
-    if (e.which === 1) {
-        // let r = randomInt(0, 255);
-        // let g = randomInt(0, 255);
-        // let b = randomInt(0, 255);
-        if (eraseMode) {
-            e.target.style.backgroundColor = 'rgb(255,255,255)';
-        } else {
-            if (rainbowMode) {
-                rgb = nextColorGradient(.3, .3, .3, 0, 2, 4)
-                e.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-            } else {
-                e.target.style.backgroundColor = drawColor;
-            }
-        }
-    }
-}
-
-// https://nicoguaro.github.io/posts/cyclic_colormaps/
-// https://krazydad.com/tutorials/makecolors.php
-let colorIndex = 0;
-
-function nextColorGradient(frequency1, frequency2, frequency3,
-    phase1, phase2, phase3,
-    center, width, len) {
-    if (center == undefined) center = 128;
-    if (width == undefined) width = 127;
-    if (len == undefined) len = 50;
-
-    let red = Math.sin(frequency1 * colorIndex + phase1) * width + center;
-    let grn = Math.sin(frequency2 * colorIndex + phase2) * width + center;
-    let blu = Math.sin(frequency3 * colorIndex + phase3) * width + center;
-    if (colorIndex > 50) {
-        colorIndex = 0;
-    }
-    colorIndex++;
-    return [red, grn, blu];
-}
-
-const grid = document.getElementById('grid');
-const colorPicker = document.getElementById('colorpicker');
-const gridSlider = document.getElementById('slider')
-const rainbow = document.getElementById('rainbow');
-const monotone = document.getElementById('monotone')
-const refresh = document.querySelector('#refresh-button');
-const newGrid = document.querySelector('#grid-button');
-
-let gridSize = parseInt(gridSlider.value);
-let style = getComputedStyle(grid);
-let width = parseInt(style.width.replace('px', ''));
-let elementSize = width / gridSize;
-let cells;
-let drawColor = '#ff0000'
-
-newGrid.addEventListener('click', setGridSizeFromPrompt);
-refresh.addEventListener('click', refreshCanvas);
-colorPicker.addEventListener('change', pickColor);
-gridSlider.addEventListener('change', setGridSizeFromSlider);
-rainbow.addEventListener('click', toggleRainbow);
-monotone.addEventListener('click', toggleMonotone);
-
-
-function pickColor() {
-    drawColor = colorPicker.value;
-    monotoneMode = true;
-    rainbowMode = false;
-    // console.log(drawColor);
-}
-
-
-// hot keys
-let eraseMode = false;
-let gridMode = false;
-let monotoneMode = false;
-let rainbowMode = false;
-window.addEventListener('keydown', checkHotKeys);
+// hot key callbacks
 
 function checkHotKeys(e) {
-    // console.log(e);
     if (e.key === 'e') toggleEraser();
     if (e.key === 'g') toggleGrid();
     if (e.key === 'm') toggleMonotone();
@@ -183,5 +186,5 @@ function toggleRainbow() {
     }
 }
 
-// setGridSize();
-initializeCanvas();
+
+initializeCanvas(gridSize);
